f7077d97b845861bd4119999c29022f6891caf42
Ahmad wali Mohammadi <ahmadwalimohammadi11@gmail.com>
2025-04-13 21:31:26 +0430

    decrement add

--- src/Components/Event/InlineHandler.svelte
+++ src/Components/Event/InlineHandler.svelte
@@ -4,9 +4,13 @@
     function handleClick() {
         count += 1;
     }
+    function handleClick1(){
+        count -= 1;
+    }
 </script>
 
 <div>
     <p>Count: {count}</p>
     <button on:click={handleClick}>Increment</button>
+    <button on:click={handleClick1}>Decrement</button>
 </div>
